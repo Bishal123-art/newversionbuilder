717c928265c9f197ab4ee33d4a4c8ea665500744
D Poovarasan <poovarasan.d@digitalshark.in>
2022-11-05 12:33:29 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./component/navbar";
 import Banner from "./pages/banner";
 import MiddleComponent from "./pages/MiddleComponent";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Banner />
       <MiddleComponent />
-    </Router>
+    </>
   );
 }
 export default App;
